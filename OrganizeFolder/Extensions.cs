7b9f5011cf5117b52c99abb61f0081b56a1abcbc
JGadd74 <jon-k-g94@hotmail.com>
2020-06-02 20:12:56 -0400

    added library of file extensions

--- OrganizeFolder/Extensions.cs
+++ OrganizeFolder/Extensions.cs
@@ -2,34 +2,36 @@
 {
     public static class Extensions
     {
-        public static string[] audio = new string[] { };
-
-        public static string[] compresssed = new string[] { };
-
-        public static string[] diskMedia = new string[] { };
-
-        public static string[] email = new string[] { };
-
-        public static string[] executable = new string[] { };
-
-        public static string[] Font = new string[] { };
-
-        public static string[] image = new string[] { };
-
-        public static string[] internet = new string[] { };
-
-        public static string[] presentation = new string[] { };
-
-        public static string[] programming = new string[] { };
-
-        public static string[] spreadsheat = new string[] { };
-
-        public static string[] system = new string[] { };
-
-        public static string[] video = new string[] { };
-
-        public static string[] wordProcessor = new string[] { };
 
 
+        public static string[] audio = new string[] {"Audio" , ".aif", ".cda" , ".mid" , ".midi" , ".mp3" , ".mpa" , ".ogg" , ".wav" , ".wma" , ".wpl"};
+
+        public static string[] compresssed = new string[] {"Compressed" , ".7z" , ".arj" , ".deb" , ".pkg" , ".rar" , ".rpm" , ".tar.gz" , ".z" , ".zip"};
+
+        public static string[] diskMedia = new string[] {"Disk/Media" , ".bin" , ".dmg" , ".iso" , ".toast" , ".vcd"};
+
+        public static string[] database = new string[] { "Database", ".csv", ".dat", ".db", ".dbf", ".log", ".mdb", ".sav", ".sql", ".tar", ".xml"};
+
+        public static string[] email = new string[] { "Email", ".email", ".eml" , ".emlx" , ".msg" , ".oft" , ".ost" , ".pst" , ".vcf"};
+
+        public static string[] executable = new string[] { "Executable", ".apk", ".bat" , ".bin" , ".cgi" , ".pl" , ".com" , ".exe" , ".gadget" , ".jar" , ".msi" , ".py" , ".wsf"};
+
+        public static string[] Font = new string[] { "Font", ".fnt", ".fon" , ".otf" , ".ttf"};
+
+        public static string[] image = new string[] { "Image", ".ai", ".bmp" , ".gif" , ".ico" , ".jpeg" , ".jpg" , ".png", ".ps" , ".psd" , ".svg" , ".tif" , ".tiff"};
+
+        public static string[] internet = new string[] { "Internet", ".asp", ".aspx" , ".cer" , ".cfm" , ".cgi" , ".pl" , ".css" , ".htm" , ".html" , ".js" , ".jsp" , ".part" , ".php" , ".py" , ".rss" , ".xhtml"};
+
+        public static string[] presentation = new string[] { "Presentation", ".key", ".odp" , ".pps" , ".ppt" , ".pptx" };
+
+        public static string[] programming = new string[] {"Programming" , ".c" , ".class" , ".cpp" , ".cs" , ".h" , ".java" , ".pl" , ".sh" , ".swift" , ".vb"};
+
+        public static string[] spreadsheet = new string[] { "Spreadsheet", ".ods", ".xls" , ".xlsm" , ".xlsx"};
+
+        public static string[] system = new string[] { "System", ".bak", ".cab" , ".cfg" , ".cpl" , ".cur" , ".dll" , ".dmp" , ".drv" , ".icns" , ".ico" , ".ini" , ".lnk" , ".msi" , ".sys" , ".tmp"};
+
+        public static string[] video = new string[] { "Video", ".3g2", ".3gp" , ".avi" , ".flv" , ".h264" , ".m4v" , ".mkv" , ".mov" , ".mp4" , ".mpg" , ".mpeg" , ".rm" , ".swf" , ".vob" , ".wmv"};
+
+        public static string[] wordProcessor = new string[] { "Word Processor", ".doc", ".docx" , ".odt" , ".pdf" , ".rtf" , ".tex" , ".txt" , ".wpd"};
     }
 }
