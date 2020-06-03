fa6323d98c40a21a9d0e74217622f89e40fbdd86
JGadd74 <jon-k-g94@hotmail.com>
2020-06-03 18:38:26 -0400

    Clean up

--- OrganizeFolder/Organizer.cs
+++ OrganizeFolder/Organizer.cs
@@ -8,16 +8,11 @@ using FileExtensions;
 
 namespace OrganizeFolder
 {
+    // goal: organize loose files into folders based on filetype
+    // ex. organize downloads folder into images, docs and videos folders etc
+    // currently only targeted at downloads folder
     public class Organizer
-    {
-        // goal: organize loose files into folders based on filetype
-        // ex. organize downloads folder into images, docs and videos folders etc
-
-        // Suggestion: Let user define categories by name and extensions
-
-
-
-
+    { 
         public Menu MainMenu = new Menu(MenuType.ScrollInput, "PSI. Folder Organizer");
         public Menu SettingsMenu = new Menu(MenuType.ScrollInput, "Settings");
         public Menu HelpMenu = new Menu(MenuType.ScrollInput, "Help");
@@ -28,40 +23,30 @@ namespace OrganizeFolder
         private static string activeUser = Environment.UserName;
 
         private static string Main = @"C:\Users\" + activeUser + @"\Downloads";
-        private static string Videos = Path.Combine(Main, "Videos");
-        private static string Images = Path.Combine(Main, "Images");
-        private static string Executables = Path.Combine(Main, "Executables");
-        private static string ISOs = Path.Combine(Main, "ISOs");
-        private static string Compressed = Path.Combine(Main, "Compressed Files");
-        private static string PDFs = Path.Combine(Main, "PDFs");
-        private static string Shortcuts = Path.Combine(Main, "Shortcuts");
-        private static string Other = Path.Combine(Main, "Other");
+        private static string Other = Path.Combine(Main, "Unknown type");
 
         public List<string> Directories = new List<string>();
         ExtensionsKit Ekit = new ExtensionsKit();
         private static IEnumerable<string> Files = Directory.EnumerateFiles(Main);
 
-
-
         public Organizer()
         {
-            
             populateDirectories();
             populateAboutFileExtensionsMenu();
 
-            MainMenu.addMethod(organizeByDefaultsExtended, "Organize Downloads");
+            MainMenu.addMethod(organizeByDefaults, "Organize Downloads");
             MainMenu.addMethod(SettingsMenu.runMenu, "Settings");
             MainMenu.addMethod(MainMenu.exitMenuLoop, "Exit");
+
             SettingsMenu.addMethod(SettingsMenu.exitMenuLoop, "Return");
             SettingsMenu.addMethod(HelpMenu.runMenu, "Help");
+
             HelpMenu.addMethod(HelpMenu.exitMenuLoop, "Return");
             HelpMenu.addMethod(TutorialMenu.runMenu, "Tutorial");
             HelpMenu.addMethod(AboutFileExtensionsMenu.runMenu, "File Exensions");
             HelpMenu.addMethod(AboutOrganizerMenu.runMenu, "About PSI. Organizer");
         }
 
-
-       
         public void populateDirectories()
         {
             foreach (string name in Ekit.getCategoryNames())
@@ -70,8 +55,6 @@ namespace OrganizeFolder
             }
             Directories.Add(Other);
         }
-
-        //List<Menu> AboutFileExtensionMenus = new List<Menu>();
 
         public void populateAboutFileExtensionsMenu()
         {
@@ -92,22 +75,10 @@ namespace OrganizeFolder
                 Menu tMenu = new Menu(MenuType.TextBody, category[0]);
                 tMenu.SetBodyText(ExtensionsColumn);
                 AboutFileExtensionsMenu.addMethod(tMenu.runMenu, category[0]);
-
             }
-            
         }
-        public bool test()
-        {
-            return true;
-        }
-        /// <summary>
-        /// for loop for iterater
-        /// create textonly menu
-        /// add text[i] to body
-        /// add method to aboutfileextensions menu 
-        /// </summary>
-        /// <returns></returns>
-        public bool organizeByDefaultsExtended()
+        
+        public bool organizeByDefaults()
         {
             foreach (string[] category in Ekit.ExtensionCategories)
             {
@@ -129,57 +100,5 @@ namespace OrganizeFolder
             }
            return true;
         }
-
-       
-
-        public bool OrganizeByDefaults()
-        {
-            foreach (string file in Files) 
-            {
-                string fileName = Path.GetFileName(file);
-                if (Path.GetExtension(file) == ".avi" || Path.GetExtension(file) == ".mp4" || Path.GetExtension(file) == ".mkv")
-                {
-                    if (!Directory.Exists(Videos)) Directory.CreateDirectory(Videos);
-                    File.Move(file, Path.Combine(Videos, fileName));
-                }
-                else if (Path.GetExtension(file) == ".jpg")
-                {
-                    if (!Directory.Exists(Images)) Directory.CreateDirectory(Images);
-                    File.Move(file, Path.Combine(Images, fileName));
-                }
-                else if (Path.GetExtension(file) == ".exe")
-                {
-                    if (!Directory.Exists(Executables)) Directory.CreateDirectory(Executables);
-                    File.Move(file, Path.Combine(Executables, fileName));
-                }
-                else if (Path.GetExtension(file) == ".iso")
-                {
-                    if (!Directory.Exists(ISOs)) Directory.CreateDirectory(ISOs);
-                    File.Move(file, Path.Combine(ISOs, fileName));
-                }
-                else if (Path.GetExtension(file) == ".zip" || Path.GetExtension(file) == ".rar" || Path.GetExtension(file) == ".7z")
-                {
-                    if (!Directory.Exists(Compressed)) Directory.CreateDirectory(Compressed);
-                    File.Move(file, Path.Combine(Compressed, fileName));
-                }
-                else if (Path.GetExtension(file) == ".pdf")
-                {
-                    if (!Directory.Exists(PDFs)) Directory.CreateDirectory(PDFs);
-                    File.Move(file, Path.Combine(PDFs, fileName));
-                }
-                else if (Path.GetExtension(file) == ".lnk")
-                {
-                    if (!Directory.Exists(Shortcuts)) Directory.CreateDirectory(Shortcuts);
-                    File.Move(file, Path.Combine(Shortcuts, fileName));
-                }
-                else
-                {
-                    if (!Directory.Exists(Other)) Directory.CreateDirectory(Other);
-                    File.Move(file, Path.Combine(Other, fileName));
-                }
-            }
-            return true;
-        }
-                   
     }
 }
